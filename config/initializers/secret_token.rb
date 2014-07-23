4ed0e65b1a10b756b4150d6fc9228ce8363601f5
frolin <frolin@frolin.(none)>
2014-07-23 21:53:55 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BursaProject::Application.config.secret_key_base = '967cfb92e1ee9f58eaec23a51862831cfb290bd9e5067e21f5c9f1c4a4689049aad5099c9987c8456ee411ff9e35ab9215a99a1f43403f15e71be871ea39ff30'
